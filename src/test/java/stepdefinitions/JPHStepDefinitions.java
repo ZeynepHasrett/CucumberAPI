c165d829c811767b5fe9af3bfe1bac7a670d37ee
ZeynepHasrett <zeynephasret.kirbas@hotmail.com>
2025-04-11 09:46:39 +0300

    zeynepv

--- src/test/java/stepdefinitions/JPHStepDefinitions.java
+++ src/test/java/stepdefinitions/JPHStepDefinitions.java
@@ -1,7 +1,22 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.*;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import utilities.ConfigReader;
+
 public class JPHStepDefinitions {
 
+    String url="";
+    Response response;
+    JsonPath resJP;
+    JSONObject reqBody;
 
+    @Given("kullanici {string} adresine gider")
+    public void kullanici_adresine_gider(String configBaseUrl) {
+
+        url+= ConfigReader.getProperty(configBaseUrl);
+    }
 
 }
