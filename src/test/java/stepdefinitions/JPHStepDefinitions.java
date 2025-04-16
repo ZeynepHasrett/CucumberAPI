f20cc1bee1c4c92ba1d5164af39e262c95b76100
ZeynepHasrett <zeynephasret.kirbas@hotmail.com>
2025-04-16 09:31:07 +0300

    v1

--- src/test/java/stepdefinitions/JPHStepDefinitions.java
+++ src/test/java/stepdefinitions/JPHStepDefinitions.java
@@ -7,10 +7,11 @@ import org.json.JSONObject;
 import utilities.ConfigReader;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JPHStepDefinitions {
 
-    String url="";
+    String url = "";
     Response response;
     JsonPath resJP;
     JSONObject reqBody;
@@ -18,17 +19,22 @@ public class JPHStepDefinitions {
     @Given("kullanici {string} adresine gider")
     public void kullanici_adresine_gider(String configBaseUrl) {
 
-        url+= ConfigReader.getProperty(configBaseUrl);
+        url += ConfigReader.getProperty(configBaseUrl);
     }
 
     @Then("kullanici path parametresi olarak {string} girer")
     public void kullanici_path_parametresi_olarak_girer(String pathParams) {
-        url=url+"/"+pathParams;
+        url = url + "/" + pathParams;
     }
 
     @Then("kullanici bir GET request yapar ve response degerleri kaydedilir")
     public void kullanici_bir_get_request_yapar_ve_response_degerleri_kaydedilir() {
-        response= given().when().get(url);
+        response = given().when().get(url);
+    }
+
+    @Then("kullanici response statusCode degerinin {int} oldugunu test eder")
+    public void kullanici_response_status_code_degerinin_oldugunu_test_eder(int expStatusCode) {
+        assertEquals(expStatusCode, response.getStatusCode());
     }
 
 }
