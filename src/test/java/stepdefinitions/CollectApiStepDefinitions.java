2d73227993ae5ba59197af61f300faa68bd8052b
ZeynepHasrett <zeynephasret.kirbas@hotmail.com>
2025-05-05 17:54:42 +0300

    zynpv

--- src/test/java/stepdefinitions/CollectApiStepDefinitions.java
+++ src/test/java/stepdefinitions/CollectApiStepDefinitions.java
@@ -1,7 +1,16 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.restassured.response.Response;
+
 public class CollectApiStepDefinitions {
 
+    String url="";
+    Response response;
 
+    @Given("kullanici sorgu icin {string} adresine gider")
+    public void kullaniciSorguIcinAdresineGider(String configBaseUrl) {
+        url+= ConfigReader.getProperty(configBaseUrl);
+    }
 
 }
